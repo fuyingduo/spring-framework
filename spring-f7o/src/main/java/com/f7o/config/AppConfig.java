e19380bb1ba16f24d1fb79158165825afb82390f
fuyd <18249585208@163.com>
2020-06-16 13:44:55 +0800

    first commit

---
+++ spring-f7o/src/main/java/com/f7o/config/AppConfig.java
@@ -0,0 +1,12 @@
+package com.f7o.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * created by fuyd on 2020-06-16
+ */
+@Configuration
+@ComponentScan("com.f7o")
+public class AppConfig {
+}
---
+++ spring-f7o/src/main/java/com/f7o/services/IndexService.java
@@ -0,0 +1,15 @@
+package com.f7o.services;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * created by fuyd on 2020-06-16
+ */
+@Service
+public class IndexService {
+
+
+	public void query() {
+		System.out.println("indexService");
+	}
+}
